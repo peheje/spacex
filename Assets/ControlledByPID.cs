8e5d069e84c6850730c5c5a9233c3695656226f1
Peter Helstrup Jensen <peterhj@gmail.com>
2017-12-21 17:14:02 +0100

    pid height

--- Assets/ControlledByPID.cs
+++ Assets/ControlledByPID.cs
@@ -3,26 +3,38 @@ using UnityEngine;
 
 public class ControlledByPID : MonoBehaviour {
 
+    public GameObject floor;
     private Rigidbody body;
     private PIDController xPID = new PIDController();
     private PIDController yPID = new PIDController();
     private PIDController zPID = new PIDController();
+    private PIDController wPID = new PIDController();
 
-    public float kp = 0.0f;
-    public float ki = 0.0f;
-    public float kd = 0.0f;
+    public float kp = 5.0f;
+    public float ki = 0.01f;
+    public float kd = 150.0f;
+
+    public float kpw = 0.0f;
+    public float kiw = 0.0f;
+    public float kdw = 0.0f;
+    private float floorPosition;
 
     void OnValidate()
     {
         // Clamp public PID coefficients
-        kp = Mathf.Clamp(kp, 0.0f, kp);
-        ki = Mathf.Clamp(ki, 0.0f, ki);
-        kd = Mathf.Clamp(kd, 0.0f, kd);
+        kp = Mathf.Max(kp, 0.0f);
+        ki = Mathf.Max(ki, 0.0f);
+        kd = Mathf.Max(kd, 0.0f);
+
+        kpw = Mathf.Max(kpw, 0.0f);
+        kiw = Mathf.Max(kiw, 0.0f);
+        kdw = Mathf.Max(kdw, 0.0f);
     }
 
     void Start () {
-        // UnityEditor.EditorWindow.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+        UnityEditor.EditorWindow.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
 
+        floorPosition = floor.transform.position.y;
         body = GetComponent<Rigidbody>();
         Debug.Log("controlled by pid");
     }
@@ -33,21 +45,28 @@ public class ControlledByPID : MonoBehaviour {
         var dx = body.rotation.x - target;
         var dy = body.rotation.y - target;
         var dz = body.rotation.z - target;
+        var dw = body.position.y - floorPosition;
+
+        Debug.Log("dw" + dw);
 
         // Get control
         var xControl = xPID.GetControl(dx, kp, ki, kd);
         var yControl = yPID.GetControl(dy, kp, ki, kd);
         var zControl = zPID.GetControl(dz, kp, ki, kd);
 
-        // Limit control
-        const float maxVal = 1.0f;
-        xControl = Mathf.Clamp(xControl, -maxVal, maxVal);
-        yControl = Mathf.Clamp(yControl, -maxVal, maxVal);
-        zControl = Mathf.Clamp(zControl, -maxVal, maxVal);
+        var wControl = wPID.GetControl(dw, kpw, kiw, kdw);
 
-        Debug.Log("force x,z: " + xControl + " " + zControl);
+        // Limit control
+        const float maxGimbal = 1.0f;
+        xControl = Mathf.Clamp(xControl, -maxGimbal, maxGimbal);
+        yControl = Mathf.Clamp(yControl, -maxGimbal, maxGimbal);
+        zControl = Mathf.Clamp(zControl, -maxGimbal, maxGimbal);
+
+        const float maxPower = 10000.0f;
+        wControl = Mathf.Clamp(wControl, -maxPower, maxPower);
 
         // Apply control
         body.AddRelativeTorque(xControl, yControl, zControl, ForceMode.Force);
+        body.AddForce(0, -wControl, 0, ForceMode.Force);
     }
 }
